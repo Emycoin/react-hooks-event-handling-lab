c46895e39b83f00f1e03b00088f6a84a8a5e22de
Emycoin <emytausevich@gmail.com>
2021-07-22 13:38:43 -0400

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnme(){
+    return(
+        <div>
+            <button onFocus={(event)=>{
+                console.log('Good!')
+            }} onBlur={(event)=>{
+                console.log('Hey! Eyes on me!')
+            }}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnme;
+
